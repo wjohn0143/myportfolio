aead5e925cdf97924c041c4148d6900b4a62cc36
Dev <Johnstar0143@gmail.com>
2024-05-02 13:20:20 -0400

    fixspace

--- app/components/helper/glow-card.jsx
+++ app/components/helper/glow-card.jsx
@@ -5,7 +5,6 @@ const GlowCard = ({ children , identifier}) => {
   useEffect(() => {
     const CONTAINER = document.querySelector(`.glow-container-${identifier}`);
     const CARDS = document.querySelectorAll(`.glow-card-${identifier}`);
-
     const CONFIG = {
       proximity: 40,
       spread: 80,
@@ -14,11 +13,9 @@ const GlowCard = ({ children , identifier}) => {
       vertical: false,
       opacity: 0,
     };
-
     const UPDATE = (event) => {
       for (const CARD of CARDS) {
         const CARD_BOUNDS = CARD.getBoundingClientRect();
-
         if (
           event?.x > CARD_BOUNDS.left - CONFIG.proximity &&
           event?.x < CARD_BOUNDS.left + CARD_BOUNDS.width + CONFIG.proximity &&
@@ -29,44 +26,33 @@ const GlowCard = ({ children , identifier}) => {
         } else {
           CARD.style.setProperty('--active', CONFIG.opacity);
         }
-
         const CARD_CENTER = [
           CARD_BOUNDS.left + CARD_BOUNDS.width * 0.5,
           CARD_BOUNDS.top + CARD_BOUNDS.height * 0.5,
         ];
-
         let ANGLE =
           (Math.atan2(event?.y - CARD_CENTER[1], event?.x - CARD_CENTER[0]) *
             180) /
           Math.PI;
-
         ANGLE = ANGLE < 0 ? ANGLE + 360 : ANGLE;
-
         CARD.style.setProperty('--start', ANGLE + 90);
       }
     };
-
     document.body.addEventListener('pointermove', UPDATE);
-
     const RESTYLE = () => {
       CONTAINER.style.setProperty('--gap', CONFIG.gap);
       CONTAINER.style.setProperty('--blur', CONFIG.blur);
       CONTAINER.style.setProperty('--spread', CONFIG.spread);
-      CONTAINER.style.setProperty(
-        '--direction',
-        CONFIG.vertical ? 'column' : 'row'
-      );
+      CONTAINER.style.setProperty('--direction', CONFIG.vertical ? 'column' : 'row');
     };
 
     RESTYLE();
     UPDATE();
-
     // Cleanup event listener
     return () => {
       document.body.removeEventListener('pointermove', UPDATE);
     };
   }, [identifier]);
-
   return (
     <div className={`glow-container-${identifier} glow-container`}>
       <article className={`glow-card glow-card-${identifier} h-fit cursor-pointer border border-[#2a2e5a] transition-all duration-300 relative bg-[#101123] text-gray-200 rounded-xl hover:border-transparent w-full`}>
--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -13,7 +13,6 @@ function Navbar() {
             JOHN SMITH
           </Link>
         </div>
-
         <ul className="mt-4 flex h-screen max-h-0 w-full flex-col items-start text-sm opacity-0 md:mt-0 md:h-auto md:max-h-screen md:w-auto md:flex-row md:space-x-1 md:border-0 md:opacity-100" id="navbar-default">
           <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#about">
@@ -28,8 +27,7 @@ function Navbar() {
           </li>
           <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#education"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EDUCATION</div></Link>
-          </li>
-          
+          </li>       
           <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></Link>
           </li>
